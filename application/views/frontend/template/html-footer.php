3480c6536fa42118d660f56b8b74aa86d2ddc90f
Hercules Lima <hl3058317@gmail.com>
2018-07-12 22:18:46 -0300

    14. Preparando o template master do Front-end do Blog 15. Preparando o template master do Back-end do Blog

---
+++ application/views/frontend/template/html-footer.php
@@ -0,0 +1,9 @@
+    <!-- jQuery -->
+    <script src="<?php base_url('assets/frontend/js/jquery.js') ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php base_url('assets/frontend/js/bootstrap.min.js') ?>"></script>
+
+</body>
+
+</html>
