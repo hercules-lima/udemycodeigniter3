9547d3ea09ae9609d0fc3b0f4a822b2024fead8f
Hercules Lima <hl3058317@gmail.com>
2018-07-13 23:50:53 -0300

    22. Carregando o título e o cabeçalho das páginas de forma dinâmica

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,6 +15,12 @@ class Home extends CI_Controller {
 		$dados['categorias'] = $this->categorias;
 		$this->load->model('publicacoes_model','modelpublicacoes');
 		$dados['postagem'] = $this->modelpublicacoes->destaques_home();
+
+		//Dados a serem enviados para o Cabeçalho
+
+		$dados['titulo']= 'Página Inicial';
+		$dados['subtitulo']= 'Postagens Recentes';
+
 		$this->load->view('frontend/template/html-header',$dados);
 		$this->load->view('frontend/template/header');
 		$this->load->view('frontend/home');
